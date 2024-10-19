26e851ae727d0ec864aeb32ac3679daa611f36a9
Gonza Gomez <gonzalo.gomez.agg@gmail.com>
2024-10-18 22:16:10 -0300

    a

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ const root = createRoot(rootElement);
 // Renderiza la aplicación
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/vite-project">
+    <BrowserRouter basename="/vite-project/">
     <App />
     </BrowserRouter>
   </StrictMode>
